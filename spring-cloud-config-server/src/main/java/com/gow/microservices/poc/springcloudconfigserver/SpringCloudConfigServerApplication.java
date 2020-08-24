d60fc19ceac6ea1703574af8e1f2b3a9a8267272
Gowdhamraj <gowdhamramaraj@gmail.com>
2020-08-25 00:16:26 +0530

    Update config repo uri

--- spring-cloud-config-server/src/main/java/com/gow/microservices/poc/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/gow/microservices/poc/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.gow.microservices.poc.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
