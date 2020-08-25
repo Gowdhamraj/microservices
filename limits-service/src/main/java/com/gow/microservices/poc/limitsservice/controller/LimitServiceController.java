46711250ed1f600af8fb178ee05b7f9a0a38ba9c
Gowdhamraj <gowdhamramaraj@gmail.com>
2020-08-26 00:01:19 +0530

    Add currency exchange service

--- limits-service/src/main/java/com/gow/microservices/poc/limitsservice/controller/LimitServiceController.java
+++ limits-service/src/main/java/com/gow/microservices/poc/limitsservice/controller/LimitServiceController.java
@@ -3,10 +3,8 @@ package com.gow.microservices.poc.limitsservice.controller;
 import com.gow.microservices.poc.limitsservice.configuration.Configuration;
 import com.gow.microservices.poc.limitsservice.model.LimitsConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
