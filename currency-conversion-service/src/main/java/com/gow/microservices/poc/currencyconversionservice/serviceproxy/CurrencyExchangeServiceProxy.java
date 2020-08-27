284cf2181709f35b3fa5a664c9a00f022b6d940d
Gowdhamraj <gowdhamramaraj@gmail.com>
2020-08-28 00:59:38 +0530

    Add load balancing and naming server for service registry
    Add Ribbon and Eureka support

--- currency-conversion-service/src/main/java/com/gow/microservices/poc/currencyconversionservice/serviceproxy/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/gow/microservices/poc/currencyconversionservice/serviceproxy/CurrencyExchangeServiceProxy.java
@@ -1,12 +1,14 @@
 package com.gow.microservices.poc.currencyconversionservice.serviceproxy;
 
 import com.gow.microservices.poc.currencyconversionservice.model.CurrencyConversionBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "currency-exchange-service", url = "localhost:8001", path = "/currency-exchange")
+//@FeignClient(name = "currency-exchange-service", url = "localhost:8001", path = "/currency-exchange")
+@FeignClient(name = "currency-exchange-service", path = "/currency-exchange")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
     @GetMapping(path = "/from/{from}/to/{to}")
